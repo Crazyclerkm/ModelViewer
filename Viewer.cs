59b1296a1a99e566084bcb056beb95ba6e25869a
Callum <cdan879@aucklanduni.ac.nz>
2025-04-03 00:13:09 +1300

    Initial program setup

---
+++ Viewer.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace ModelViewer.Core {
+    public class Viewer : GameWindow {
+        public Viewer(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() {
+            ClientSize = (width, height),
+            Title = title
+        }) { }
+
+        static void Main() {
+            using(Viewer viewer = new Viewer(800, 600, "Model Viewer")) {
+                viewer.Run();
+            }
+        }
+    }
+}
